0b16e100b68c00c3206a0b509cd89571421e3f6f
Josh <joshhh980@gmail.com>
2026-08-31 15:13:26 +0000

    feat: remove price check

--- admin/sales/manage_sale.php
+++ admin/sales/manage_sale.php
@@ -256,10 +256,10 @@ if(isset($_GET['id'])){
                 return;
             }
             const costPrice = costs[item] || 0
-            if(price < costPrice){
-                alert_toast('Item price is less than cost price.','error');
-                return;
-            }
+            //if(price < costPrice){
+            //    alert_toast('Item price is less than cost price.','error');
+            //    return;
+            //}
             var total = parseFloat(qty) * parseFloat(price)
             // console.log(supplier,item)
             var item_name = items[item].name || 'N/A';
